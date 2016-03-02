69d6326e5878b94e069fd4349bff07e86c80fd3e
js345 <1994yxf@gmail.com>
2016-03-02 16:09:35 -0600

    fix stem plot

--- hw4/p3.5.py
+++ hw4/p3.5.py
@@ -9,14 +9,11 @@ import numpy as np
 df = pd.read_csv("wine.data.txt", sep=",", skipinitialspace=True, skiprows=0, header=None).as_matrix()
 x = df[:,1:]
 Y = df[:,0]
-covr = np.cov(x.T)
+covr = np.cov(x.T,bias=1)
 w,v = np.linalg.eig(covr)
-w,v = zip(*sorted(zip(w, v),reverse=True))
-
-#plt.hlines(1,1,20)
-#plt.eventplot(w1, orientation= 'horizontal', colors = 'b')
-#plt.axis('off')
-#plt.show()
+idx = w.argsort()[::-1]
+w = w[idx]
+v = v[:,idx]
 
 # eigenvalue plot
 plt.plot(np.arange(len(w)),w)
@@ -26,7 +23,7 @@ plt.show()
 # first 3 pc
 for i in range(3):
     eigenv = w[i]
-    y = v[i]
+    y = v[:,i]
     x = np.arange(len(y))
     plt.stem(x, y, linefmt='b-', markerfmt='bo', basefmt='r-')
     plt.title(eigenv)
