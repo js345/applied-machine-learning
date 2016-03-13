0f13e41dd59e97c495cec1daaba0f0969394880c
js345 <1994yxf@gmail.com>
2016-03-12 21:36:16 -0600

    Fix data loading bug
    Fixed python list initiation and added gitignore

--- hw5/part1.py
+++ hw5/part1.py
@@ -9,14 +9,15 @@ def load_data():
     total_word_count = int(lines[2])
 
     i = 3
-    output = [[0]*word_num]*doc_count
+    output = [[0]*word_num for i in range(doc_count)]
     while i < len(lines):
-        line = lines[i].split(' ')
+        line = lines[i].strip().split(' ')
         doc_id = int(line[0])
         word_id = int(line[1])
         word_count = int(line[2])
-        output[doc_id][word_id] = word_count
-
+        output[doc_id-1][word_id-1] = word_count
         i += 1
-
     return np.array(output)
+
+if __name__ == '__main__':
+    print load_data()
