d630c8ea2192e2c94798fe2b1889d95a6d186721
Yiyi Wang <shd101wyy@sina.com>
2016-03-13 15:01:55 -0500

    change to class

--- hw5/part1.py
+++ hw5/part1.py
@@ -1,23 +1,27 @@
 import numpy as np
 
-def load_data():
-    f = open('./docword.nips.txt', 'r')
-    lines = f.readlines()
 
-    doc_count = int(lines[0])
-    word_num = int(lines[1])
-    total_word_count = int(lines[2])
+class EM:
+    def __init__(self, file_name): #'./docword.nips.txt'
+        f = open(file_name, 'r')
+        lines = f.readlines()
 
-    i = 3
-    output = [[0]*word_num for i in range(doc_count)]
-    while i < len(lines):
-        line = lines[i].strip().split(' ')
-        doc_id = int(line[0])
-        word_id = int(line[1])
-        word_count = int(line[2])
-        output[doc_id-1][word_id-1] = word_count
-        i += 1
-    return np.array(output)
+        self.doc_count = int(lines[0])
+        self.word_num = int(lines[1])
+        self.total_word_count = int(lines[2])
+
+        i = 3
+        output = [[0]*word_num for i in range(doc_count)]
+        while i < len(lines):
+            line = lines[i].strip().split(' ')
+            doc_id = int(line[0])
+            word_id = int(line[1])
+            word_count = int(line[2])
+            output[doc_id-1][word_id-1] = word_count
+            i += 1
+            
+        self.data = np.array(output)
+
 
 if __name__ == '__main__':
-    print load_data()
+    # print load_data()
