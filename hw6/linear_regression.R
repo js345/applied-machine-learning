488b3bd7e81c981d698ef765f140d39dba15ba70
js345 <1994yxf@gmail.com>
2016-04-02 15:11:49 -0500

    linear regression and box cox in notebook

--- hw6/linear_regression.R
+++ hw6/linear_regression.R
@@ -2,3 +2,9 @@ efd<-as.matrix(read.table('./Geographical Original of Music/default_plus_chromat
 x<-efd[,-(1:2)]
 latitude<-efd[,ncol(efd)-1]
 longitude<-efd[,ncol(efd)]
+# linear regression - latitdue as y
+y<-latitude
+foo<-data.frame(x=x,y=y)
+foo.lm<-lm(y~x,data=foo)
+plot(y)
+abline(foo.lm)
