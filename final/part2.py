d309e4a07fb353891597aac22d551d4037d5836a
js345 <1994yxf@gmail.com>
2016-05-04 19:06:15 -0500

    added report template

--- final/part2.py
+++ final/part2.py
@@ -57,21 +57,21 @@ def train():
     def max_pool_2x2(x):
         return tf.nn.max_pool(x, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
-    W_conv1 = weight_variable([5, 5, 1, 8])
-    b_conv1 = bias_variable([8])
+    W_conv1 = weight_variable([5, 5, 1, 16])
+    b_conv1 = bias_variable([16])
 
     x_image = tf.reshape(x, [-1,28,28,1])
 
     h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1)
     h_pool1 = max_pool_2x2(h_conv1)
 
-    W_conv2 = weight_variable([5, 5, 8, 64])
-    b_conv2 = bias_variable([64])
+    W_conv2 = weight_variable([5, 5, 16, 32])
+    b_conv2 = bias_variable([32])
 
     h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2)
     h_pool2 = max_pool_2x2(h_conv2)
 
-    W_conv3 = weight_variable([5, 5, 64, 64])
+    W_conv3 = weight_variable([5, 5, 32, 64])
     b_conv3 = bias_variable([64])
 
     h_conv3 = tf.nn.relu(conv2d(h_pool2, W_conv3) + b_conv3)
